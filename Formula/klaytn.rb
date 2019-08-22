15227705bad64d20872ca8454b555e76bd822d35
Aidan <aidan.kwon@groundx.xyz>
2019-08-22 21:53:43 +0900

    add more binaries

--- Formula/klaytn.rb
+++ Formula/klaytn.rb
@@ -19,21 +19,37 @@ class Klaytn < Formula
     system "make", "ken"
     bin.install "build/bin/ken"
     
+    system "make", "kbn"
+    bin.install "build/bin/kbn"
+    
     system "make", "kscn"
     bin.install "build/bin/kscn"
+    
+    system "make", "kspn"
+    bin.install "build/bin/kspn"
+    
+    system "make", "ksen"
+    bin.install "build/bin/ksen"
     
     system "make", "kgen"
     bin.install "build/bin/kgen"
     
     system "make", "homi"
     bin.install "build/bin/homi"
+    
+    system "make", "abigen"
+    bin.install "build/bin/abigen"
   end
 
   test do
     system "kcn", "version"
     system "kpn", "version"
     system "ken", "version"
+    system "kbn", "version"
     system "kscn", "version"
+    system "kspn", "version"
+    system "ksen", "version"
     system "kgen", "version"
+    system "homi", "--version"
   end
 end
