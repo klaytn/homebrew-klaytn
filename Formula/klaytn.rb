f13e473d641c702a0775db6a4e636b448f3543b6
Donghwan Aidan Kwon <48199072+aidan-kwon@users.noreply.github.com>
2019-10-21 17:45:56 +0900

    Update klaytn version to 1.2.0(#2)

--- Formula/klaytn.rb
+++ Formula/klaytn.rb
@@ -1,7 +1,7 @@
 class Klaytn < Formula
   desc "Klaytn Blockchain Platform"
   homepage "https://docs.klaytn.com"
-  url "https://github.com/klaytn/klaytn.git", :tag => "v1.1.1"
+  url "https://github.com/klaytn/klaytn.git", :tag => "v1.2.0"
 
   head "https://github.com/klaytn/klaytn.git", :branch => "master"
 
